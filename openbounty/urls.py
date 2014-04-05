9f0ded2d0586f0c5cfe3879560f821f9f3881a55
Vincent Chen <vincent@vincent-VirtualBox.(none)>
2014-04-05 04:28:05 -0400

    Created tempaltes placeholders

--- openbounty/urls.py
+++ openbounty/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import patterns, url
 from openbounty import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.index, name='index')
+    url(r'^$', views.index, name='index'),
+    url(r'^create', views.create, name='Create Challenge')
 )

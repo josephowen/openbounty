9aa8bdb686d9c534d2b16f18819c4a91f37c89cf
Vincent Chen <irnanc@gmail.com>
2014-04-05 16:13:45 -0400

    ugly ugly comments now show up

--- openbounty/views.py
+++ openbounty/views.py
@@ -1,8 +1,8 @@
+import datetime
 from django.shortcuts import render
-from django.http import HttpResponse
-from openbounty.models import Challenge, BountyUser
-from openbounty.forms import ChallengeForm
-from django.core.exceptions import ObjectDoesNotExist 
+from django.http import HttpResponse, HttpResponseRedirect
+from openbounty.models import Challenge, BountyUser, Comment
+from openbounty.forms import ChallengeForm, CommentForm
 # Create your views here.
 
 def index(request):
@@ -12,18 +12,44 @@ def index(request):
 def create(request):
     form = ChallengeForm()
     if request.method == 'POST':
-	form = ChallengeForm(request.POST)
-	if form.is_valid() and request.user.is_authenticated():
-	    bounty = form.cleaned_data['bounty']
-	    title = form.cleaned_data['title']
-	    challenge = form.cleaned_data['challenge']
-	    expiration_date = form.cleaned_data['expiration_date']
-	    challenge_object = Challenge.objects.create(user=request.user,bounty=bounty,title=title,challenge=challenge,expiration_date=expiration_date)
-	
-	elif not request.user.is_authenticated():
-	    return HttpResponse("You need to login");
+        form = ChallengeForm(request.POST)
+    if form.is_valid() and request.user.is_authenticated():
+        bounty = form.cleaned_data['bounty']
+        title = form.cleaned_data['title']
+        challenge = form.cleaned_data['challenge']
+        expiration_date = form.cleaned_data['expiration_date']
+        challenge_object = Challenge.objects.create(user=request.user,bounty=bounty,title=title,challenge=challenge,expiration_date=expiration_date)     
+        return HttpResponseRedirect('')     
+    elif not request.user.is_authenticated():
+        return HttpResponse("You need to login");
 
     context = {}
-    context['challenges'] = Challenge.objects.all()
     context['form'] = form
     return render(request, 'openbounty/create.html', context)
+
+def view_challenges(request):
+    form = CommentForm()
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+    if form.is_valid() and request.user.is_authenticated():
+        title = form.cleaned_data['title']
+        comment = form.cleaned_data['comment']
+        challenge_id = request.POST['challenge_id']
+        challenge = Challenge.objects.get(id = challenge_id)
+        user = request.user
+        date_posted = datetime.datetime.now()
+        comment_object = Comment.objects.create(user=user,title=title,comment=comment,challenge=challenge,date_posted=date_posted)
+        return HttpResponseRedirect('')
+
+    elif not request.user.is_authenticated():
+        return HttpResponse("You need to login");
+    context = {}    
+    challenges = Challenge.objects.all()
+    context['contents'] = []
+    for challenge in challenges:
+        data = {}        
+        data['challenge'] = challenge       
+        data['comments'] = len(Comment.objects.filter(challenge=challenge))
+        context['contents'].append(data)
+    context['form'] = form
+    return render(request, 'openbounty/view.html', context)

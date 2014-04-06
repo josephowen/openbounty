b0a2417721bec5fc97927235dfd86843e7d0d9d4
xiagu <xiagu@xiagu.co>
2014-04-06 08:19:06 -0400

    Final migrations

--- openbounty/migrations/0001_initial.py
+++ openbounty/migrations/0001_initial.py
@@ -11,7 +11,7 @@ class Migration(SchemaMigration):
         # Adding model 'Challenge'
         db.create_table(u'openbounty_challenge', (
             (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('bounty', self.gf('django.db.models.fields.FloatField')()),
+            ('bounty', self.gf('django.db.models.fields.DecimalField')(max_digits=500, decimal_places=2)),
             ('title', self.gf('django.db.models.fields.CharField')(max_length=120)),
             ('challenge', self.gf('django.db.models.fields.CharField')(max_length=2000)),
             ('post_date', self.gf('django.db.models.fields.DateTimeField')(default=datetime.datetime.now)),
@@ -34,7 +34,10 @@ class Migration(SchemaMigration):
             ('is_staff', self.gf('django.db.models.fields.BooleanField')(default=False)),
             ('is_active', self.gf('django.db.models.fields.BooleanField')(default=True)),
             ('date_joined', self.gf('django.db.models.fields.DateTimeField')(default=datetime.datetime.now)),
-            ('phone_number', self.gf('django.db.models.fields.CharField')(max_length=15)),
+            ('phone_number', self.gf('django.db.models.fields.CharField')(max_length=15, blank=True)),
+            ('wallet', self.gf('django.db.models.fields.DecimalField')(default=0, max_digits=500, decimal_places=2)),
+            ('access_token', self.gf('django.db.models.fields.CharField')(default=None, max_length=100, blank=True)),
+            ('venmo', self.gf('django.db.models.fields.CharField')(default='', max_length=60, blank=True)),
         ))
         db.send_create_signal(u'openbounty', ['BountyUser'])
 
@@ -64,16 +67,25 @@ class Migration(SchemaMigration):
         ))
         db.send_create_signal(u'openbounty', ['Backing'])
 
-        # Adding model 'Comment'
-        db.create_table(u'openbounty_comment', (
+        # Adding model 'Proof'
+        db.create_table(u'openbounty_proof', (
             (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
             ('user', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['openbounty.BountyUser'])),
             ('challenge', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['openbounty.Challenge'])),
-            ('title', self.gf('django.db.models.fields.CharField')(max_length=120)),
-            ('comment', self.gf('django.db.models.fields.CharField')(max_length=2000)),
-            ('date_posted', self.gf('django.db.models.fields.DateTimeField')(auto_now_add=True, blank=True)),
+            ('url', self.gf('django.db.models.fields.CharField')(max_length=100, blank=True)),
+            ('description', self.gf('django.db.models.fields.CharField')(max_length=5000)),
+            ('votes', self.gf('django.db.models.fields.IntegerField')(default=0)),
+            ('winner', self.gf('django.db.models.fields.BooleanField')(default=False)),
         ))
-        db.send_create_signal(u'openbounty', ['Comment'])
+        db.send_create_signal(u'openbounty', ['Proof'])
+
+        # Adding model 'ClaimVotes'
+        db.create_table(u'openbounty_claimvotes', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('user', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['openbounty.BountyUser'])),
+            ('claim', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['openbounty.Proof'])),
+        ))
+        db.send_create_signal(u'openbounty', ['ClaimVotes'])
 
 
     def backwards(self, orm):
@@ -92,8 +104,11 @@ class Migration(SchemaMigration):
         # Deleting model 'Backing'
         db.delete_table(u'openbounty_backing')
 
-        # Deleting model 'Comment'
-        db.delete_table(u'openbounty_comment')
+        # Deleting model 'Proof'
+        db.delete_table(u'openbounty_proof')
+
+        # Deleting model 'ClaimVotes'
+        db.delete_table(u'openbounty_claimvotes')
 
 
     models = {
@@ -125,6 +140,7 @@ class Migration(SchemaMigration):
         },
         u'openbounty.bountyuser': {
             'Meta': {'object_name': 'BountyUser'},
+            'access_token': ('django.db.models.fields.CharField', [], {'default': 'None', 'max_length': '100', 'blank': 'True'}),
             'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
             'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
             'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
@@ -136,15 +152,18 @@ class Migration(SchemaMigration):
             'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
             'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
             'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
-            'phone_number': ('django.db.models.fields.CharField', [], {'max_length': '15'}),
+            'phone_number': ('django.db.models.fields.CharField', [], {'max_length': '15', 'blank': 'True'}),
             'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'related_name': "u'user_set'", 'blank': 'True', 'to': u"orm['auth.Permission']"}),
-            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
+            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'}),
+            'venmo': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '60', 'blank': 'True'}),
+            'wallet': ('django.db.models.fields.DecimalField', [], {'default': '0', 'max_digits': '500', 'decimal_places': '2'})
         },
         u'openbounty.challenge': {
             'Meta': {'object_name': 'Challenge'},
             'backers': ('django.db.models.fields.related.ManyToManyField', [], {'related_name': "'backings'", 'symmetrical': 'False', 'through': u"orm['openbounty.Backing']", 'to': u"orm['openbounty.BountyUser']"}),
-            'bounty': ('django.db.models.fields.FloatField', [], {}),
+            'bounty': ('django.db.models.fields.DecimalField', [], {'max_digits': '500', 'decimal_places': '2'}),
             'challenge': ('django.db.models.fields.CharField', [], {'max_length': '2000'}),
+            'doers': ('django.db.models.fields.related.ManyToManyField', [], {'related_name': "'proofs'", 'symmetrical': 'False', 'through': u"orm['openbounty.Proof']", 'to': u"orm['openbounty.BountyUser']"}),
             'expiration_date': ('django.db.models.fields.DateTimeField', [], {}),
             'fulfilled': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
             u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
@@ -152,14 +171,22 @@ class Migration(SchemaMigration):
             'title': ('django.db.models.fields.CharField', [], {'max_length': '120'}),
             'user': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['openbounty.BountyUser']"})
         },
-        u'openbounty.comment': {
-            'Meta': {'object_name': 'Comment'},
-            'challenge': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['openbounty.Challenge']"}),
-            'comment': ('django.db.models.fields.CharField', [], {'max_length': '2000'}),
-            'date_posted': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+        u'openbounty.claimvotes': {
+            'Meta': {'object_name': 'ClaimVotes'},
+            'claim': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['openbounty.Proof']"}),
             u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'title': ('django.db.models.fields.CharField', [], {'max_length': '120'}),
             'user': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['openbounty.BountyUser']"})
+        },
+        u'openbounty.proof': {
+            'Meta': {'object_name': 'Proof'},
+            'challenge': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['openbounty.Challenge']"}),
+            'description': ('django.db.models.fields.CharField', [], {'max_length': '5000'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'url': ('django.db.models.fields.CharField', [], {'max_length': '100', 'blank': 'True'}),
+            'user': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['openbounty.BountyUser']"}),
+            'voters': ('django.db.models.fields.related.ManyToManyField', [], {'related_name': "'claim_votes'", 'symmetrical': 'False', 'through': u"orm['openbounty.ClaimVotes']", 'to': u"orm['openbounty.BountyUser']"}),
+            'votes': ('django.db.models.fields.IntegerField', [], {'default': '0'}),
+            'winner': ('django.db.models.fields.BooleanField', [], {'default': 'False'})
         }
     }
 

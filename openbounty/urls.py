90afc8b348be27c98afddd6be16d95314ca553ad
Joseph Owen <josephowen92@gmail.com>
2014-04-06 01:21:20 -0400

    Fixed link to create new challange, so that appears

--- openbounty/urls.py
+++ openbounty/urls.py
@@ -5,7 +5,7 @@ from openbounty import views, loginviews, profileview, debugviews
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
     #views.create should eventuallly be in it's own class, like createviews or something
-    url(r'^create', views.create, name='create_challenge'),
+    url(r'^create_challenge', views.create, name='create_challenge'),
     url(r'^view_challenges', views.view_challenges, name='view_challenges'),
     url(r'^challenge/(?P<challenge_id>\d*)', views.challenge, name='challenge'),
     url(r'^login', loginviews.login, name='login'),
